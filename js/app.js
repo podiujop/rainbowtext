3578163b3dbc6effdbdd59c776902a8a13d9a8b1
podiujop <podebradyujop@yandex.ru>
2016-04-01 03:21:16 +0200

    Go!

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
 	$("#textSubmit").click(function (){
 
-		$("#output").empty();
+		$("#textOutput").empty();
 		$("#codeOutput").empty();
 		
 		var inputText = $("#textInput").val();
@@ -12,12 +12,26 @@ $(document).ready(function () {
 			var g = Math.floor(Math.random() * 255) + 1;
 			var b = Math.floor(Math.random() * 255) + 1;
 
-			$(".rainbow").append('<span style="color: rgb(' + r + "," + g + "," + b + ')">' + inputText[i] + '</span>');
+			$("#textOutput").append('<span style="color: rgb(' + r + "," + g + "," + b + ')">' + inputText[i] + '</span>');
 		};
 
 		var codeText = $("#textOutput").html();
 		$("#codeOutput").val(codeText);
 
+	});
+
+	$(".header__home-link").mouseenter(function () {
+
+		$(this).empty();
+		var homeLink = "Rainbow text generator"
+
+		for (var i = 0; i < homeLink.length; i++) {
+			var r = Math.floor(Math.random() * 255) + 1;
+			var g = Math.floor(Math.random() * 255) + 1;
+			var b = Math.floor(Math.random() * 255) + 1;
+
+			$(this).append('<span style="color: rgb(' + r + "," + g + "," + b + ')">' + homeLink[i] + '</span>');
+		};
 	});
 
 });
